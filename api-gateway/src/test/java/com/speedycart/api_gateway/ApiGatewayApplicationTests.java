c3cccbab1ef4ddfb2eb968229e60bbdd9a5ad4ee
iParthDubey <par0765dub@gmail.com>
2025-07-02 23:46:39 +0530

    API GateWay

---
+++ api-gateway/src/test/java/com/speedycart/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speedycart.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
